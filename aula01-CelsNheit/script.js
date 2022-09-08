acc9d56cb5134801b211f5ff523409dd5cc895fd
pramim7antofaz@gmail.com <pramim7antofaz@gmail.com>
2022-09-08 10:48:47 -0300

    CelNheit pode ser FahSius

--- aula01-CelsNheit/script.js
+++ aula01-CelsNheit/script.js
@@ -1,11 +1,16 @@
 var campoResultado = document.getElementById("resultado");
 
-function Converter(){
-  var celsius = document.getElementById("valorInputado").value;
-  
-  if (celsius == "") celsius = 0;
-  
-  var fahrenheit = (celsius * 1.8 + 32).toFixed(1)
-  
-  campoResultado.innerHTML = `${celsius} °C é equivalente a ${fahrenheit} °F.`
+function CPraF(){
+  var c = document.getElementById("valorInputado").value;
+  if (c == "") c = 0;
+  var f = (c * 1.8 + 32).toFixed(1)
+  campoResultado.innerHTML = `${c} °C é equivalente a ${f} °F.`
 }
+
+function FPraC(f){
+  var f = document.getElementById("valorInputado").value;
+  if (f == "") f = 0;
+  var c = ((f - 32) * (5/9)).toFixed(1);
+  campoResultado.innerHTML = `${f} °F é equivalente a ${c} °C`
+}
+
